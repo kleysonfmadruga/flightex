0985c68450adeba30e01614709712899a1dd0907
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-08-06 21:47:11 -0300

    Adds Flightex.Users.User moduledoc

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,4 +1,9 @@
 defmodule Flightex.Users.User do
+  @moduledoc ~S"""
+  User is a struct that represents an user that have
+  a name, an email and a CPF in the Flightex application.
+  """
+
   @keys [:name, :email, :cpf, :id]
   @enforce_keys @keys
   defstruct @keys
